7089c22f6d2831a33a13bbab5bd3c772f19ddd6f
Cjkjvfnby <Cjkjvfnby@gmail.com>
2015-04-10 03:12:37 +0400

    Handle handlers for config file in  module.

--- default/AI/freeorion_debug/handlers/__init__.py
+++ default/AI/freeorion_debug/handlers/__init__.py
@@ -1,1 +1,8 @@
 import charts_handler
+from freeorion_debug.option_tools import get_option_dict, HANDLERS
+
+handlers = get_option_dict()[HANDLERS].split()
+
+for handler in handlers:
+    __import__('freeorion_debug.handlers.%s' % handler)
+
